3545b802f841ac495ba9479a012bc4eced6df53b
sbomb16 <sbomb16@users.noreply.github.com>
2020-03-06 00:46:30 -0700

    Pathfinding and eventbus DEV

---
+++ Path Finding And Event Bus/Assets/Engine/Scripts/Cannon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    private bool mIsQuitting;
+    public Rigidbody rb;
+
+    private void OnEnable()
+    {
+        EventBus.StartListening("shoot", ShootCannon);
+    }
+
+    private void OnApplicationQuit()
+    {
+        mIsQuitting = true;
+    }
+
+    private void OnDisable()
+    {
+        if(mIsQuitting == false)
+        {
+            EventBus.EndListening("shoot", ShootCannon);
+        }
+    }
+
+    void ShootCannon()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.AddForce(new Vector3(0, 500, 500));
+        Debug.Log("Shot the cannon!");
+    }
+}
---
+++ Path Finding And Event Bus/Assets/Engine/Scripts/EventPublisher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPublisher : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("s"))
+        {
+            EventBus.TriggerEvent("shoot");
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            EventBus.TriggerEvent("drop");
+        }
+        if (Input.GetKeyDown("g"))
+        {
+            EventBus.TriggerEvent("inverse");
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            EventBus.TriggerEvent("expand");
+        }
+	}
+}
