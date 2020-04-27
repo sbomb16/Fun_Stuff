d0c3b66d81593b907e713d4d4bea8b091ae12930
Spencer Bentley <sbentley189@gmail.com>
2020-04-26 21:55:41 -0700

    ?

--- Ballistics and Decorating/Assets/AI/Scripts/SteeringBehavior.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SteeringBehavior {
-
-    public abstract SteeringOutput GetSteering();
-	
-}
